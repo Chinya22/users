16cc2a5f03f80b63164b46f07c9b8523f4379cc8
chinya22 <anastasia220899@gmail.com>
2024-06-21 17:17:19 +0300

    cards

---
+++ src/js/index.js
@@ -0,0 +1,373 @@
+const users = [
+  {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+    address: {
+      street: "Kulas Light",
+      suite: "Apt. 556",
+      city: "Gwenborough",
+      zipcode: "92998-3874",
+      geo: {
+        lat: "-37.3159",
+        lng: "81.1496",
+      },
+    },
+    phone: "1-770-736-8031 x56442",
+    website: "hildegard.org",
+    company: {
+      name: "Romaguera-Crona",
+      catchPhrase: "Multi-layered client-server neural-net",
+      bs: "harness real-time e-markets",
+    },
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+    username: "Antonette",
+    email: "Shanna@melissa.tv",
+    address: {
+      street: "Victor Plains",
+      suite: "Suite 879",
+      city: "Wisokyburgh",
+      zipcode: "90566-7771",
+      geo: {
+        lat: "-43.9509",
+        lng: "-34.4618",
+      },
+    },
+    phone: "010-692-6593 x09125",
+    website: "anastasia.net",
+    company: {
+      name: "Deckow-Crist",
+      catchPhrase: "Proactive didactic contingency",
+      bs: "synergize scalable supply-chains",
+    },
+  },
+  {
+    id: 3,
+    name: "Clementine Bauch",
+    username: "Samantha",
+    email: "Nathan@yesenia.net",
+    address: {
+      street: "Douglas Extension",
+      suite: "Suite 847",
+      city: "McKenziehaven",
+      zipcode: "59590-4157",
+      geo: {
+        lat: "-68.6102",
+        lng: "-47.0653",
+      },
+    },
+    phone: "1-463-123-4447",
+    website: "ramiro.info",
+    company: {
+      name: "Romaguera-Jacobson",
+      catchPhrase: "Face to face bifurcated interface",
+      bs: "e-enable strategic applications",
+    },
+  },
+  {
+    id: 4,
+    name: "Patricia Lebsack",
+    username: "Karianne",
+    email: "Julianne.OConner@kory.org",
+    address: {
+      street: "Hoeger Mall",
+      suite: "Apt. 692",
+      city: "South Elvis",
+      zipcode: "53919-4257",
+      geo: {
+        lat: "29.4572",
+        lng: "-164.2990",
+      },
+    },
+    phone: "493-170-9623 x156",
+    website: "kale.biz",
+    company: {
+      name: "Robel-Corkery",
+      catchPhrase: "Multi-tiered zero tolerance productivity",
+      bs: "transition cutting-edge web services",
+    },
+  },
+  {
+    id: 5,
+    name: "Chelsey Dietrich",
+    username: "Kamren",
+    email: "Lucio_Hettinger@annie.ca",
+    address: {
+      street: "Skiles Walks",
+      suite: "Suite 351",
+      city: "Roscoeview",
+      zipcode: "33263",
+      geo: {
+        lat: "-31.8129",
+        lng: "62.5342",
+      },
+    },
+    phone: "(254)954-1289",
+    website: "demarco.info",
+    company: {
+      name: "Keebler LLC",
+      catchPhrase: "User-centric fault-tolerant solution",
+      bs: "revolutionize end-to-end systems",
+    },
+  },
+  {
+    id: 6,
+    name: "Mrs. Dennis Schulist",
+    username: "Leopoldo_Corkery",
+    email: "Karley_Dach@jasper.info",
+    address: {
+      street: "Norberto Crossing",
+      suite: "Apt. 950",
+      city: "South Christy",
+      zipcode: "23505-1337",
+      geo: {
+        lat: "-71.4197",
+        lng: "71.7478",
+      },
+    },
+    phone: "1-477-935-8478 x6430",
+    website: "ola.org",
+    company: {
+      name: "Considine-Lockman",
+      catchPhrase: "Synchronised bottom-line interface",
+      bs: "e-enable innovative applications",
+    },
+  },
+  {
+    id: 7,
+    name: "Kurtis Weissnat",
+    username: "Elwyn.Skiles",
+    email: "Telly.Hoeger@billy.biz",
+    address: {
+      street: "Rex Trail",
+      suite: "Suite 280",
+      city: "Howemouth",
+      zipcode: "58804-1099",
+      geo: {
+        lat: "24.8918",
+        lng: "21.8984",
+      },
+    },
+    phone: "210.067.6132",
+    website: "elvis.io",
+    company: {
+      name: "Johns Group",
+      catchPhrase: "Configurable multimedia task-force",
+      bs: "generate enterprise e-tailers",
+    },
+  },
+  {
+    id: 8,
+    name: "Nicholas Runolfsdottir V",
+    username: "Maxime_Nienow",
+    email: "Sherwood@rosamond.me",
+    address: {
+      street: "Ellsworth Summit",
+      suite: "Suite 729",
+      city: "Aliyaview",
+      zipcode: "45169",
+      geo: {
+        lat: "-14.3990",
+        lng: "-120.7677",
+      },
+    },
+    phone: "586.493.6943 x140",
+    website: "jacynthe.com",
+    company: {
+      name: "Abernathy Group",
+      catchPhrase: "Implemented secondary concept",
+      bs: "e-enable extensible e-tailers",
+    },
+  },
+  {
+    id: 9,
+    name: "Glenna Reichert",
+    username: "Delphine",
+    email: "Chaim_McDermott@dana.io",
+    address: {
+      street: "Dayna Park",
+      suite: "Suite 449",
+      city: "Bartholomebury",
+      zipcode: "76495-3109",
+      geo: {
+        lat: "24.6463",
+        lng: "-168.8889",
+      },
+    },
+    phone: "(775)976-6794 x41206",
+    website: "conrad.com",
+    company: {
+      name: "Yost and Sons",
+      catchPhrase: "Switchable contextually-based project",
+      bs: "aggregate real-time technologies",
+    },
+  },
+  {
+    id: 10,
+    name: "Clementina DuBuque",
+    username: "Moriah.Stanton",
+    email: "Rey.Padberg@karina.biz",
+    address: {
+      street: "Kattie Turnpike",
+      suite: "Suite 198",
+      city: "Lebsackbury",
+      zipcode: "31428-2261",
+      geo: {
+        lat: "-38.2386",
+        lng: "57.2232",
+      },
+    },
+    phone: "024-648-3804",
+    website: "ambrose.net",
+    company: {
+      name: "Hoeger LLC",
+      catchPhrase: "Centralized empowering task-force",
+      bs: "target end-to-end models",
+    },
+  },
+];
+
+function createUser(user) {
+  const cardsWrapper = document.querySelector(".cards_wrapper");
+  const card = document.createElement("div");
+  card.classList.add("cards_wrapper_item");
+
+  const img = document.createElement("img");
+  img.src = "img/" + user.id + ".png";
+  img.classList.add("cards_wrapper_item_img");
+  card.append(img);
+
+  const infoWrapper = document.createElement("div");
+  infoWrapper.classList.add("cards_wrapper_item_info");
+  card.append(infoWrapper);
+
+  const name = document.createElement("div");
+  name.classList.add("cards_wrapper_item_info_name");
+  name.innerText = user.name;
+  const nameSpan = document.createElement("span");
+  nameSpan.innerText = "Name: ";
+  name.prepend(nameSpan);
+  infoWrapper.append(name);
+
+  const username = document.createElement("div");
+  username.classList.add("cards_wrapper_item_info_username");
+  username.innerText = user.username;
+  const usernameSpan = document.createElement("span");
+  usernameSpan.innerText = "Username: ";
+  username.prepend(usernameSpan);
+  infoWrapper.append(username);
+
+  const email = document.createElement("div");
+  email.classList.add("cards_wrapper_item_info_email");
+  email.innerText = user.email;
+  const emailSpan = document.createElement("span");
+  emailSpan.innerText = "Email: ";
+  email.prepend(emailSpan);
+  infoWrapper.append(email);
+
+  const adressWrapper = document.createElement("div");
+  adressWrapper.classList.add("cards_wrapper_item_info_adress");
+  infoWrapper.append(adressWrapper);
+
+  const street = document.createElement("div");
+  street.classList.add("cards_wrapper_item_info_adress_street");
+  street.innerText = user.address.street;
+  const streetSpan = document.createElement("span");
+  streetSpan.innerText = "Street: ";
+  street.prepend(streetSpan);
+  adressWrapper.append(street);
+
+  const suite = document.createElement("div");
+  suite.classList.add("cards_wrapper_item_info_adress_suite");
+  suite.innerText = user.address.suite;
+  const suiteSpan = document.createElement("span");
+  suiteSpan.innerText = "Suite: ";
+  suite.prepend(suiteSpan);
+  adressWrapper.append(suite);
+
+  const city = document.createElement("div");
+  city.classList.add("cards_wrapper_item_info_adress_city");
+  city.innerText = user.address.city;
+  const citySpan = document.createElement("span");
+  citySpan.innerText = "City: ";
+  city.prepend(citySpan);
+  adressWrapper.append(city);
+
+  const zip = document.createElement("div");
+  zip.classList.add("cards_wrapper_item_info_adress_zip");
+  zip.innerText = user.address.zipcode;
+  const zipSpan = document.createElement("span");
+  zipSpan.innerText = "Zip code: ";
+  zip.prepend(zipSpan);
+  adressWrapper.append(zip);
+
+  const phone = document.createElement("div");
+  phone.classList.add("cards_wrapper_item_info_phone");
+  phone.innerText = user.phone;
+  const phoneSpan = document.createElement("span");
+  phoneSpan.innerText = "Phone: ";
+  phone.prepend(phoneSpan);
+  infoWrapper.append(phone);
+
+  const site = document.createElement("div");
+  site.classList.add("cards_wrapper_item_info_site");
+  site.innerText = user.website;
+  const siteSpan = document.createElement("span");
+  siteSpan.innerText = "Site: ";
+  site.prepend(siteSpan);
+  infoWrapper.append(site);
+
+  const characteristicsWrapper = document.createElement("div");
+  characteristicsWrapper.classList.add(
+    "cards_wrapper_item_info_characteristics"
+  );
+  infoWrapper.append(characteristicsWrapper);
+
+  const company = document.createElement("div");
+  company.classList.add("cards_wrapper_item_info_characteristics_company");
+  company.innerText = user.company.name;
+  const companySpan = document.createElement("span");
+  companySpan.innerText = "Company: ";
+  company.prepend(companySpan);
+  characteristicsWrapper.append(company);
+
+  const prase = document.createElement("div");
+  prase.classList.add("cards_wrapper_item_info_characteristics_catch");
+  prase.innerText = user.company.catchPhrase;
+  const praseSpan = document.createElement("span");
+  praseSpan.innerText = "Catch prase: ";
+  prase.prepend(praseSpan);
+  characteristicsWrapper.append(prase);
+
+  const bs = document.createElement("div");
+  bs.classList.add("cards_wrapper_item_info_characteristics_bs");
+  bs.innerText = user.company.bs;
+  const bsSpan = document.createElement("span");
+  bsSpan.innerText = "BS: ";
+  bs.prepend(bsSpan);
+  characteristicsWrapper.append(bs);
+
+  const buttonsWrapper = document.createElement("div");
+  buttonsWrapper.classList.add("cards_wrapper_item_info_buttons");
+  infoWrapper.append(buttonsWrapper);
+
+  const buttonEdit = document.createElement("button");
+  buttonEdit.classList.add("cards_wrapper_item_info_buttons_item");
+  buttonEdit.innerText = "Edit";
+  buttonsWrapper.append(buttonEdit);
+
+  const buttonDelete = document.createElement("button");
+  buttonDelete.classList.add("cards_wrapper_item_info_buttons_item");
+  buttonDelete.innerText = "Delete";
+  buttonsWrapper.append(buttonDelete);
+
+  cardsWrapper.append(card);
+}
+
+for (let i = 0; i < users.length; i++) {
+  createUser(users[i]);
+}
